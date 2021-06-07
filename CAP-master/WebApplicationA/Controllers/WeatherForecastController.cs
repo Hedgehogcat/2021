f94c7a7c189fb181ff4d3ec54b822f4f2fd9708e
liyi <774773015@qq.com>
2021-06-07 16:33:26 +0800

    添加 WebApplicationA

---
+++ CAP-master/WebApplicationA/Controllers/WeatherForecastController.cs
@@ -0,0 +1,66 @@
+﻿using Grpc.Net.Client;
+using GrpcServiceA;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationA.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public class msgBack
+        {
+            public msgBack(string head, string body, string code)
+            {
+                Head = head;
+                Body = body;
+                Code = code;
+            }
+            public msgBack() { }
+           public string Head { get; set; }
+           public string Body { get; set; }
+          public  string Code { get; set; }
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpPost]
+        public async Task <msgBack> Create(string id)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "晓晨" });
+            msgBack back = new msgBack(id,"Greeter 服务返回数据: " + reply.Message,"success");
+            return back;
+        }
+        
+    }
+   
+}
